2975eea744f139b6138ac1df65acca3817401eb3
변현준 <jam6754@gmail.com>
2026-02-26 12:26:32 +0900

    chore: 프로젝트 초기 세팅 (Unity 3D URP + 폴더구조 + gitignore + editorconfig)

---
+++ backend/src/test/java/com/ssafy/ssafyplaytime/SsafyplaytimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.ssafyplaytime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsafyplaytimeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
