116ff70272edf1b93c7cb3f1268fd1ec569f7978
kth5352 <kth5352@gmail.com>
2026-03-05 14:41:17 +0900

    [S14P21D104-100] refactor(effects): WaterMelon_Sword 리네임 및 렌더 보정

--- client/Assets/_Project/Scripts/Game/Item/Common/ItemIds.cs
+++ client/Assets/_Project/Scripts/Game/Item/Common/ItemIds.cs
@@ -8,7 +8,8 @@ namespace SSAFYPlayTime.Gameplay.Items
         public const string Americano = "ITEM_AMERICANO";
         public const string Flamethrower = "ITEM_FLAMETHROWER";
         public const string Invisibility = "ITEM_INVISIBILITY";
-        public const string OfficeTool = "ITEM_OFFICE_TOOL";
+        public const string WaterMelonSword = "ITEM_WATERMELON_SWORD";
+        public const string OfficeTool = WaterMelonSword;
         public const string SatelliteStrike = "ITEM_SATELLITE_STRIKE";
     }
 }
