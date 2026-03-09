bf4c15f47e041afd54bc31c0503d82f42fe62fb4
진윤세 <dbstp1234@naver.com>
2026-03-09 12:11:11 +0900

    [S14P21D104-109] CL/chore: 맵 에셋 추가

--- client/Assets/_Project/Scripts/Lobby/LobbyCanvasUIController.cs
+++ client/Assets/_Project/Scripts/Lobby/LobbyCanvasUIController.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -102,13 +102,11 @@ namespace SSAFYPlayTime
         [SerializeField] private TMP_Text playerTwoText;
         [SerializeField] private TMP_Text playerThreeText;
         [SerializeField] private TMP_Text playerFourText;
-        [FormerlySerializedAs("aiJiCharacterRoot")]
         [SerializeField] private GameObject ssatyCharacterRoot;
         [SerializeField] private GameObject pitCharacterRoot;
         [SerializeField] private GameObject seuTatiCharacterRoot;
         [SerializeField] private GameObject waiJeuCharacterRoot;
         [SerializeField] private GameObject characterSelectionPanel;
-        [FormerlySerializedAs("selectAiJiCharacterButton")]
         [SerializeField] private Button selectSsatyCharacterButton;
         [SerializeField] private Button selectPitCharacterButton;
         [SerializeField] private Button selectSeuTatiCharacterButton;
